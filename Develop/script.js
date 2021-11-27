61d4077d5c440b5d399c46131928d15685d2e0e6
Marcus Morua <marcus.morua@gmail.com>
2021-11-27 16:56:23 -0600

    Add code to generatePassword function ; Add vchar values to array

--- Develop/script.js
+++ Develop/script.js
@@ -1,59 +1,69 @@
 // Assignment code here
+    // chars array variables
+    var upperCases = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+    var lowerCases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    var specials = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "+"]; 
+
 function generatePassword() {
-    // array variables
-    var potentialCharactersArray =[
-      ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'], 
-      ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], 
-      [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 
-      ['!', "@", '#', '$', '%', '^', '&', '*', '-', '+']]; 
-  
-    // confirm variables
-    var userWantsNumbers = confirm(
-      "Would you like your password to include numbers?"
-    );
-    var userWantsUppercase = confirm(
-      "Would you like your password to include uppercase letters?"
-    );
-    var userWantsLowerCase = confirm(
-      "Would you like your password to include lowercase letters?"
-    );
-    var userWantsSpecialChars = confirm(
-      "Would you like your password to include special characters?"
-    );
-  
-    // if statements
-    if (userWantsNumbers) {
-      console.log("The user does want numbers.");
+    // Ask user if they'd like to use uppercase letters
+    var confirmUpperCases = confirm("Would you like to use uppercase letters?")
+    // Log a resulting message to the console so we know that it worked
+    if (confirmUpperCases === true) {
+      console.log("You chose to use uppercase letters.")
     } else {
-      console.log("The user doesn't want numbers.");
+      console.log("You chose not to use uppercase letters.")
     }
-    if (userWantsUppercase) {
-      console.log("The user does want uppercase letters.");
+
+    // Ask user if they'd like lowercase letters
+    var confirmLowerCase = confirm("Would you like to use uppercase letters?")
+    // Log a resulting message to the console so we know that it worked
+    if (confirmLowerCase === true) {
+      console.log("You chose to use lowercase letters.")
     } else {
-      console.log("The user doesn't want uppercase letters.")
+      console.log("You chose not to use lowercase letters.")
     }
-    if (userWantsLowerCase) {
-      console.log("The user does want lowercase letters.");
+
+    // Ask user if they'd like numbers 
+    var confirmNumbers = confirm("Would you like to use numbers?")
+    // Log a resulting message to the console so we know that it worked
+    if (confirmNumbers === true) {
+      console.log("You chose to use numbers.")
     } else {
-      console.log("The user doesn't want lowercase letters.")
+      console.log("You chose not to use numbers.")
     }
-    if (userWantsSpecialChars) {
-      console.log("The user does want special characters.");
+
+    // Ask user if they'd like special characters
+    var confirmSpecials = confirm("Would you like to use special characters?")
+    // Log a resulting message to the console so we know that it worked
+    if (confirmSpecials === true) {
+      console.log("You chose to use special characters.")
     } else {
-      console.log("The user doesn't want special characters.")
+      console.log("You chose not to use special characters.")
     }
+
+    // Ask user how long the password will be
+    var passwordLength = window.prompt(
+      "How long would you like the password? Please choose between 8-128 characters."
+    )
+    // Log a resulting message to the console so we know that it worked
+    console.log("This many characters")
+
 
 };
 
-// Get references to the #generate element
+
+
+// Assigning a variable to the generate button
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
 };
 
-// Add event listener to generate button
+// Calls the writePassword function when the generate password is clicked
 generateBtn.addEventListener("click", writePassword);
