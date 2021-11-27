dc7078ac445f354b50c3f7872dc1b406d1f40fe7
Marcus Morua <marcus.morua@gmail.com>
2021-11-26 20:45:04 -0600

    Add code to generatePassword function()

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,40 @@
-// Assignment code here
+  // Assignment code here
+function generatePassword() {
+  var array = [];
+
+  /* gather user information
+  1. Do they want numbers? - Done
+  2. Do they want upper case chars? - Done
+  3. Do they want lower case chars? - Done
+  4. Do they want special chars? - Done
+  */
+
+  const userWantsNumbers = confirm(
+    "Would you like your password to include numbers?"
+  );
+  const userWantsUppercase = confirm(
+    "Would you like your password to include uppercase?"
+  );
+  const userWantsLowerCase = confirm(
+    "Would you like your password to include lowercase?"
+  );
+  const userWantsSpecialChars = confirm(
+    "Would you like your password to include special chars?"
+  );
+
+  if (userWantsNumbers) {
+    console.log("The user does want numbers");
+  }
+  if (userWantsUppercase) {
+    console.log("The user does want uppercase");
+  }
+  if (userWantsLowerCase) {
+    console.log("The user does want lowercase");
+  }
+  if (userWantsSpecialChars) {
+    console.log("The user does want special chars");
+  }
+}
 
 
 // Get references to the #generate element
@@ -8,6 +44,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
